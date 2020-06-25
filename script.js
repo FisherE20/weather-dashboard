e2b81d287552f6697da9050ea16c29b4367ff9d4
Erin Fisher <flybirdie85@gmail.com>
2020-06-24 21:39:53 -0700

    updated 5 day forecast

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(".search").on("click", function () {
   // This is our API key
   var APIKey = "b6c1420f672427cac6dc4bc3480d4d03";
-  var city= $('#search').val();
+  var city = $("#search").val();
   // Here we are building the URL we need to query the database
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?" +
@@ -10,7 +10,7 @@ $(".search").on("click", function () {
     "&appid=" +
     APIKey;
 
-    console.log(city);
+  console.log(city);
   // Here we run our AJAX call to the OpenWeatherMap API
   $.ajax({
     url: queryURL,
@@ -18,11 +18,11 @@ $(".search").on("click", function () {
   })
     // We store all of the retrieved data inside of an object called "response"
     .then(function (response) {
-    // Log the queryURL
+      // Log the queryURL
       console.log(queryURL);
-     // Log the resulting object
+      // Log the resulting object
       console.log(response);
-
+      console.log(city);
       $("ul").prepend(`<li>${city}</li>`);
 
       // Transfer content to HTML
@@ -38,53 +38,54 @@ $(".search").on("click", function () {
       $(".pOne").text("Temperature (F) " + tempF.toFixed(2));
 
       var APIKey = "b6c1420f672427cac6dc4bc3480d4d03";
-      var lon= response.coord.lon;
-      var lat= response.coord.lat;
+      var lon = response.coord.lon;
+      var lat = response.coord.lat;
 
-      
-        let queryURLUv =
+      let queryURLUv =
         "https://api.openweathermap.org/data/2.5/onecall?appid=" +
         APIKey +
-        "&lat=" + lat + "&lon=" + lon;
-      
-    
+        "&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+
       $.ajax({
         url: queryURLUv,
         method: "GET",
       }).then(function (res) {
         console.log(res);
-         lat= res.lat;
-         lon= res.lon;
-         $(".pFour").text("UV Index:" + res.current.uvi);
+        lat = res.lat;
+        lon = res.lon;
+        $(".pFour").text("UV Index:" + res.current.uvi);
       });
-      
-      var APIKey = "b6c1420f672427cac6dc4bc3480d4d03";
 
-      let queryURLFuture = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon=" + lon +
-      "&exclude=hourly,daily&appid=" 
-      + APIKey;
-      
+      var key = "b6c1420f672427cac6dc4bc3480d4d03";
+      var queryUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
       $.ajax({
-        url: queryURLFuture,
-        method: "GET",
-      }).then(function (res) {
-        console.log(res);
-         lat= res.lat;
-         lon= res.lon;
-         daily= res.daily;
+        url: queryUrl,
+        dataType: "json",
 
-        //  var i;
-        //  for(i = 0; i < daily.length; i++){
-        //    text += daily[i];
-        //  };
-
-      $("#futurecast").text("<p>" + daily[0] + "<p>");
-      $("#futurecast").text("<p>" + daily[1] + "<p>");
-      $("#futurecast").text("<p>" + daily[2] + "<p>");
-      $("#futurecast").text("<p>" + daily[3] + "<p>");
-      $("#futurecast").text("<p>" + daily[4] + "<p>");
+        data: {
+          q: city,
+          appid: key,
+          units: "metric",
+          cnt: "6",
+        },
+        success: function (data) {
+          console.log("Received data:", data); // For testing
+          var wf = "";
+          $.each(data.list, function (index, val) {
+            wf += "<b>Day " + index + "</b>: "; // Day
+            wf += val.main.temp + "&degC"; // Temperature
+            wf += "<span> | " + val.weather[0].description + "</span>"; // Description
+            wf +=
+              "<img src='https://openweathermap.org/img/w/" +
+              val.weather[0].icon +
+              ".png'>"; // Icon
+          });
+          $("#futureCast").html(wf);
+        },
+      });
     });
-});
-
 });
